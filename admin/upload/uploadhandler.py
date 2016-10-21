d887fdb90a0d17fa64355268231bdc85b4bb0c6a
lxh <25894829@qq.com>
2016-10-21 18:15:47 +0800

    add upload file

---
+++ admin/upload/uploadhandler.py
@@ -0,0 +1,24 @@
+# -*- oding:utf-8 -*-
+
+from admin import admin
+
+options = {
+    "uploadimage": uploadimg,
+    "uploadfile": uploadfile,
+}
+
+
+
+
+
+def uploadimg():
+    pass
+
+
+def uploadfile():
+    pass
+
+
+@admin.route("/filehandler")
+def filehandler():
+    pass
